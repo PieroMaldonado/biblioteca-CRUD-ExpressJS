b76487226297bcdeb55b5f06ded40b746948ca32
Piero Maldonado <pieromaldonado16@gmail.com>
2022-09-30 12:44:09 -0500

    Initial commit

---
+++ controllers/librosController.js
@@ -0,0 +1,12 @@
+var conexion = require('../config/conexion');
+var libro = require('../model/libro');
+
+module.exports={
+    index:function(req,res){
+        libro.obtener(conexion, function(err,datos){
+            console.log(datos);
+            res.render('libros/index', {title: 'Aplicación' });
+        });
+    }
+
+}
