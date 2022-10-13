9523e6de4e3944674c465eec993c8cb021d43693
Piero Maldonado <pieromaldonado16@gmail.com>
2022-10-13 14:32:38 -0500

    refactor: Implement middlewares

--- routes/libros.js
+++ routes/libros.js
@@ -2,42 +2,27 @@ var express = require('express');
 var router = express.Router();
 const librosController = require("../controllers/librosController");
 const {body} = require('express-validator');
-
-var multer = require('multer');
-var fecha = Date.now();
-var rutaAlmacen = multer.diskStorage({
-    destination:function (request, file, callback){
-        callback(null,'./public/images/')
-    },
-    filename:function (request, file, callback){
-        callback(null,fecha+"_"+file.originalname)
-    }
-});
-var cargar = multer({ storage:rutaAlmacen,
-    fileFilter: (req, file, cb) => {
-    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-        cb(null, true);
-    } else {
-        cb(null, false);
-        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-    }} 
-    });
+const cargar = require('../middleware/multer');
+const authentication = require('../middleware/authentication');
 
 /* GET home page. */
-router.get('/', librosController.index);
-router.get('/crear', librosController.crear);
-router.post("/",cargar.single("archivo"),[
+router.get('/', authentication.ifNotLoggedin,librosController.index);
+router.get('/crear',authentication.ifNotLoggedin, librosController.crear);
+router.post("/",authentication.ifNotLoggedin,cargar.single("archivo"),
+[
     body('nombre')
     .notEmpty()
     .withMessage('Debe escribir un nombre')
 ],
 librosController.guardar);
-router.post('/eliminar/:id',librosController.eliminar);
-router.get('/editar/:id',librosController.editar);
-router.post("/actualizar",cargar.single("archivo"),[
+router.post('/eliminar/:id', authentication.ifNotLoggedin,librosController.eliminar);
+router.get('/editar/:id', authentication.ifNotLoggedin,librosController.editar);
+router.post("/actualizar", authentication.ifNotLoggedin,cargar.single("archivo"),
+[
     body('nombre')
     .notEmpty()
     .withMessage('Debe escribir un nombre')
-],librosController.actualizar);
+],
+librosController.actualizar);
 
 module.exports = router;
